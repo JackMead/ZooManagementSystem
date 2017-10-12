68b5e95c0bfaf003e3e41f2ded62ad1f36eea112
JackMead <Jack.Mead@softwire.com>
2017-10-12 10:49:59 +0100

    Added animals

---
+++ Zoo.BusinessLogic/Models/Animals/GiantPanda.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zoo.BusinessLogic.Models.Animals
+{
+    class GiantPanda : AnimalThatCanBeGroomed
+    {
+        public GiantPanda(DateTime dateOfBirth) : base(dateOfBirth)
+        {
+            
+        }
+    }
+}
---
+++ Zoo.BusinessLogic/Models/Animals/Mammoth.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zoo.BusinessLogic.Models.Animals
+{
+    class Mammoth : Animal
+    {
+        public Mammoth(DateTime dateOfBirth) : base(dateOfBirth)
+        {
+            
+        }
+    }
+}
